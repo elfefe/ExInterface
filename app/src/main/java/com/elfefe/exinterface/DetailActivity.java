c79afe4e37286fea7b08a6f510e673e45d8a2604
elfefe <f.bou-reiff@orange.fr>
2019-03-13 19:07:08 +0100

    Replace the main activity by a fragment.

--- app/src/main/java/com/elfefe/exinterface/DetailActivity.java
+++ app/src/main/java/com/elfefe/exinterface/DetailActivity.java
@@ -1,7 +1,9 @@
 package com.elfefe.exinterface;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        //1 - Configuring Toolbar
+        this.configureToolbar();
+    }
+
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
